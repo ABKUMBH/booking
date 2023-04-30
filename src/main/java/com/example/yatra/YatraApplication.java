80f952052b46c8ed05a4b3c23811b589399270a9
Abhishek Kumbhar <abhikumbharalk@gmail.com>
2023-04-30 17:19:04 +0530

    CodePush

---
+++ src/main/java/com/example/yatra/YatraApplication.java
@@ -0,0 +1,16 @@
+package com.example.yatra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+@SpringBootApplication
+@RestController
+public class YatraApplication {
+    @GetMapping("/yatra")
+	public static void main(String[] args) {
+		SpringApplication.run(YatraApplication.class, args);
+	}
+
+}
