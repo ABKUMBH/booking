7788fd52127a540fb85d5cc45f71774dbb428bf0
Abhishek Kumbhar <abhikumbharalk@gmail.com>
2023-05-20 11:50:24 +0530

    jenkinsfile

--- src/main/java/com/example/yatra/Bus.java
+++ src/main/java/com/example/yatra/Bus.java
@@ -1,8 +1,7 @@
 package com.example.yatra;
 
-import org.springframework.boot.SpringApplication;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Bus {
--- src/main/java/com/example/yatra/Visa.java
+++ src/main/java/com/example/yatra/Visa.java
@@ -1,11 +1,13 @@
+package com.example.yatra;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-public class Visa{
+public class Visa {
     @GetMapping("/visa")
-    public String getData(){
+    public String getData() {
         return "Visa daal na bro";
     }
 }
